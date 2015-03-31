0b2fa0137e54a24f30e2270e3c85737d3e4b24cc
Neal Strong <nealrstrong@gmail.com>
2015-03-30 19:59:38 -0600

    Moving parse_csv file to parse_csv_tools Adding execution file get_course_schedule

---
+++ get_course_schedule.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import parse_csv_tools as pct
+import glob
+import syslog
+if not os.path.exists('csvs'):
+    syslog.syslog(syslog.LOG_WARNING, 'Could not locate directory csvs')
+    sys.exit(1)
+
+file_list = glob.glob('csvs/*.csv')
+course_data = {}
+user_data = {}
+enrollment_data = []
+for f in file_list:
+    fh = open(f, 'r')
+    data = pct.load_csv(fh)
+    fh.close()
+    if not data:
+        syslog.syslog(syslog.LOG_ERR, 'Failed to load csv (%s)' % (f))
+        continue
+    if data.keys()[0] == 'course':
+        course_data.update(data.values()[0])
+    elif data.keys()[0] == 'user':
+        user_data.update(data.values()[0])
+    elif data.keys()[0] == 'enrollment':
+        enrollment_data.extend(data.values()[0])
+
+course_enrollment = pct.get_active_course_schedule(course_data, user_data, enrollment_data)
+if not course_enrollment:
+    print "No active courses available"
+    sys.exit(0)
+pct.print_course_schedule(course_enrollment, course_data)
+sys.exit(0)
